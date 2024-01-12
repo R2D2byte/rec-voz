0c0dd24a261111227f3eb37600f29709b750dda2
Arturo Vazquez <artvazr2@gmail.com>
2024-01-12 13:46:39 -0600

    new person

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def train_new_data():
     y = dataset.iloc[:, -1].values
 
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 1)
 
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
